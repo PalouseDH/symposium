c36484a942c6ad6bb41afa850cc0f90dbcf79047
Evan Will <evanpeterw@gmail.com>
2018-04-04 15:58:34 -0700

    make search work

--- docs/assets/js/lunr/lunr-en.js
+++ docs/assets/js/lunr/lunr-en.js
@@ -3,10 +3,8 @@ layout: null
 ---
 
 var idx = lunr(function () {
-  this.field('title')
+  //this.field('title')
   this.field('excerpt')
-  this.field('categories')
-  this.field('tags')
   this.ref('id')
 
   this.pipeline.remove(lunr.trimmer)
@@ -15,8 +13,6 @@ var idx = lunr(function () {
     this.add({
       title: store[item].title,
       excerpt: store[item].excerpt,
-      categories: store[item].categories,
-      tags: store[item].tags,
       id: item
     })
   }
@@ -44,31 +40,15 @@ $(document).ready(function() {
     resultdiv.prepend('<p class="results__found">'+result.length+' {{ site.data.ui-text[site.locale].results_found | default: "Result(s) found" }}</p>');
     for (var item in result) {
       var ref = result[item].ref;
-      if(store[ref].teaser){
-        var searchitem =
-          '<div class="list__item">'+
-            '<article class="archive__item" itemscope itemtype="http://schema.org/CreativeWork">'+
-              '<h2 class="archive__item-title" itemprop="headline">'+
-                '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
-              '</h2>'+
-              '<div class="archive__item-teaser">'+
-                '<img src="'+store[ref].teaser+'" alt="">'+
-              '</div>'+
-              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,20).join(" ")+'...</p>'+
-            '</article>'+
-          '</div>';
-      }
-      else{
-    	  var searchitem =
-          '<div class="list__item">'+
-            '<article class="archive__item" itemscope itemtype="http://schema.org/CreativeWork">'+
-              '<h2 class="archive__item-title" itemprop="headline">'+
-                '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
-              '</h2>'+
-              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,20).join(" ")+'...</p>'+
-            '</article>'+
-          '</div>';
-      }
+      var searchitem =
+        '<div class="list__item">'+
+          '<article class="archive__item" itemscope itemtype="http://schema.org/CreativeWork">'+
+            '<h2 class="archive__item-title" itemprop="headline">'+
+              '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].url+'</a>'+
+            '</h2>'+
+            '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,20).join(" ")+'...</p>'+
+          '</article>'+
+        '</div>';
       resultdiv.append(searchitem);
     }
   });
--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -3,34 +3,10 @@ layout: null
 ---
 
 var store = [
-  {%- for c in site.collections -%}
-    {%- if forloop.last -%}
-      {%- assign l = true -%}
-    {%- endif -%}
-    {%- assign docs = c.docs | where_exp:'doc','doc.search != false' -%}
-    {%- for doc in docs -%}
-      {%- if doc.header.teaser -%}
-        {%- capture teaser -%}{{ doc.header.teaser }}{%- endcapture -%}
-      {%- else -%}
-        {%- assign teaser = site.teaser -%}
-      {%- endif -%}
+  {%- for c in site.html_pages -%}
       {
-        "title": {{ doc.title | jsonify }},
-        "excerpt":
-          {%- if site.search_full_content == true -%}
-            {{ doc.content | strip_html | strip_newlines | jsonify }},
-          {%- else -%}
-            {{ doc.content | strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-          {%- endif -%}
-        "categories": {{ doc.categories | jsonify }},
-        "tags": {{ doc.tags | jsonify }},
-        "url": {{ doc.url | absolute_url | jsonify }},
-        "teaser":
-          {%- if teaser contains "://" -%}
-            {{ teaser | jsonify }}
-          {%- else -%}
-            {{ teaser | absolute_url | jsonify }}
-          {%- endif -%}
-      }{%- unless forloop.last and l -%},{%- endunless -%}
-    {%- endfor -%}
+        //"title": {{ c.title | jsonify }},
+        "excerpt": {{ c.content | strip_html | strip_newlines | jsonify }},
+        "url": {{ c.url | absolute_url | jsonify }}
+      }{%- unless forloop.last -%},{%- endunless -%}
   {%- endfor -%}]
