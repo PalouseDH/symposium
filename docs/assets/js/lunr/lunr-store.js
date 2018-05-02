909a78d6047094ebe899577985ce5cab848ad82d
evanwill <evanpeterw@gmail.com>
2018-05-02 16:38:56 -0700

    tweak lunr search

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [
   {%- for c in site.html_pages -%}
       {
         //"title": {{ c.title | jsonify }},
-        "excerpt": {{ c.content | strip_html | strip_newlines | jsonify }},
+        "excerpt": {{ c.content | strip_html | jsonify }},
         "url": {{ c.url | absolute_url | jsonify }}
       }{%- unless forloop.last -%},{%- endunless -%}
   {%- endfor -%}]
